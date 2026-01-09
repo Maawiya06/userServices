a027f9e2ecefc4685faa564811fa7d2ecaefe0b8
Maawiya06 <maawiyaameer001@gmail.com>
2026-01-09 19:23:35 +0530

    commit

---
+++ src/main/java/com/lcwd/userservices/services/implementation/userServiceImplementation.java
@@ -0,0 +1,24 @@
+package com.lcwd.userservices.services.implementation;
+
+import com.lcwd.userservices.entities.User;
+import com.lcwd.userservices.services.userServices;
+
+import java.util.List;
+
+public class userServiceImplementation implements userServices {
+
+    @Override
+    public User saveUser(User user) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUser() {
+        return List.of();
+    }
+
+    @Override
+    public User getUser(String UserId) {
+        return null;
+    }
+}
--- src/main/java/com/lcwd/userservices/services/userServices.java
+++ src/main/java/com/lcwd/userservices/services/userServices.java
@@ -7,11 +7,14 @@ import java.util.List;
 public interface userServices {
 
     // create
-    User saveUser (User user);
+    User saveUser(User user);
 
     // get all user
     List<User> getAllUser();
 
     // get single user of given userId
     User getUser(String UserId);
+
+    //TODO: delete
+    //TODO: update
 }
