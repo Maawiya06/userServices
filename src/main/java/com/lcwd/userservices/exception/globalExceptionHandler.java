3abf72dd5816f16b8c6db6a8c5930d099aaa371f
Maawiya06 <maawiyaameer001@gmail.com>
2026-01-09 19:54:33 +0530

    commit

--- src/main/java/com/lcwd/userservices/exception/globalExceptionHandler.java
+++ src/main/java/com/lcwd/userservices/exception/globalExceptionHandler.java
@@ -1,13 +1,18 @@
 package com.lcwd.userservices.exception;
 
 import com.lcwd.userservices.payload.ApiResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class globalExceptionHandler {
 
-    public ResponseEntity<ApiResponse> handleResourceNotFoundException(){
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> handleResourceNotFoundException(ResourceNotFoundException ex){
+        String message = ex.getMessage();
+        ApiResponse build = ApiResponse.builder().message(message).success(true).status(HttpStatus.NOT_FOUND).build();
 
     }
 }
