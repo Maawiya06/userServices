6c3110b2a0736fa292bac9eef411518086853e54
Maawiya06 <maawiyaameer001@gmail.com>
2026-01-09 19:09:47 +0530

    commit

--- src/main/java/com/lcwd/userservices/repositories/userRepositories.java
+++ src/main/java/com/lcwd/userservices/repositories/userRepositories.java
@@ -4,4 +4,6 @@ import com.lcwd.userservices.entities.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface userRepositories extends JpaRepository<user, String> {
+    // you want to implement any custom method or query
+
 }
