7e8d0052f4d1d7d76a8289f704d519559c52e6f4
Maawiya06 <maawiyaameer001@gmail.com>
2026-01-09 20:01:45 +0530

    commit

--- src/main/java/com/lcwd/userservices/controller/UserController.java
+++ src/main/java/com/lcwd/userservices/controller/UserController.java
@@ -1,4 +1,13 @@
 package com.lcwd.userservices.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
 public class UserController {
+
+    // create
+    // all get
+    // single get
 }
