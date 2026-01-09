876a748ab110791e3399782802730c11e7037215
Maawiya06 <maawiyaameer001@gmail.com>
2026-01-09 19:25:20 +0530

    commit

--- src/main/java/com/lcwd/userservices/repositories/UserRepositories.java
+++ src/main/java/com/lcwd/userservices/repositories/UserRepositories.java
@@ -3,7 +3,7 @@ package com.lcwd.userservices.repositories;
 import com.lcwd.userservices.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface userRepositories extends JpaRepository<User, String> {
+public interface UserRepositories extends JpaRepository<User, String> {
     // you want to implement any custom method or query
 
 }
--- src/main/java/com/lcwd/userservices/services/implementation/userServiceImplementation.java
+++ src/main/java/com/lcwd/userservices/services/implementation/userServiceImplementation.java
@@ -1,11 +1,18 @@
 package com.lcwd.userservices.services.implementation;
 
 import com.lcwd.userservices.entities.User;
+import com.lcwd.userservices.repositories.UserRepositories;
 import com.lcwd.userservices.services.userServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class userServiceImplementation implements userServices {
+
+    @Autowired
+    private UserRepositories userRepositories;
 
     @Override
     public User saveUser(User user) {
