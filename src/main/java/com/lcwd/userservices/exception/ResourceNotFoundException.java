5344681bcd59883ffb7e2e318d5a1cfc7a31cda5
Maawiya06 <maawiyaameer001@gmail.com>
2026-01-09 19:32:58 +0530

    commit

--- src/main/java/com/lcwd/userservices/exception/ResourceNotFoundException.java
+++ src/main/java/com/lcwd/userservices/exception/ResourceNotFoundException.java
@@ -1,4 +1,12 @@
 package com.lcwd.userservices.exception;
 
 public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(){
+        super("Resource not found on server !!");
+    }
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
 }
