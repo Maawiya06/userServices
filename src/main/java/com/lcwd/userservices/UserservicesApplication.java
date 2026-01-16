771c030b432de26b780075b67a59d01677e7be52
Maawiya06 <maawiyaameer001@gmail.com>
2026-01-16 20:18:34 +0530

    commit

--- src/main/java/com/lcwd/userservices/UserservicesApplication.java
+++ src/main/java/com/lcwd/userservices/UserservicesApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Ena
 public class UserservicesApplication {
 
 	public static void main(String[] args) {
