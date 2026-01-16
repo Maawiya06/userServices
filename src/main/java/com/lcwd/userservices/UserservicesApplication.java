874b16a4ef82d426511fe345f5ec1423172c0c30
Maawiya06 <maawiyaameer001@gmail.com>
2026-01-16 21:57:40 +0530

    commit

--- src/main/java/com/lcwd/userservices/UserservicesApplication.java
+++ src/main/java/com/lcwd/userservices/UserservicesApplication.java
@@ -1,10 +1,13 @@
 package com.lcwd.userservices;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
-@Ena
+@EnableDiscoveryClient
 public class UserservicesApplication {
 
 	public static void main(String[] args) {
