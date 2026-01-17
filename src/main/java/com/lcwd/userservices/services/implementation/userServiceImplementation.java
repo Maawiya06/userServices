363338a147fd0c55b051202dd7dea1f91d53fc7a
Maawiya06 <maawiyaameer001@gmail.com>
2026-01-17 17:05:18 +0530

    Merge branch 'main' of https://github.com/Maawiya06/userServices

--- src/main/java/com/lcwd/userservices/services/implementation/userServiceImplementation.java
+++ src/main/java/com/lcwd/userservices/services/implementation/userServiceImplementation.java
@@ -1,15 +1,20 @@
 package com.lcwd.userservices.services.implementation;
 
+import com.lcwd.userservices.entities.Rating;
 import com.lcwd.userservices.entities.User;
 import com.lcwd.userservices.exception.ResourceNotFoundException;
 import com.lcwd.userservices.repositories.UserRepositories;
 import com.lcwd.userservices.services.UserServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
 
 @Service
 public class userServiceImplementation implements UserServices {
@@ -19,6 +24,8 @@ public class userServiceImplementation implements UserServices {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    private Logger logger = LoggerFactory.getLogger(userServiceImplementation.class);
 
     @Override
     public User saveUser(User user) {
@@ -41,9 +48,14 @@ public class userServiceImplementation implements UserServices {
         // get user from database with the help of user repository
         User user = userRepositories.findById(UserId).orElseThrow(() ->
                 new ResourceNotFoundException("User with given Id is not found on server !! : " + UserId));
+        
         // fetch rating of the above user from rating services
         //http://localhost:8083/ratings/users/022411c7-628b-48ed-ac68-10a1ba40baf9
-
+        
+        ArrayList<Rating> ratingOfUsers = restTemplate.getForObject("http://localhost:8083/ratings/users/" + user.getUserId(), ArrayList.class);
+        logger.info("{} ", ratingOfUsers);
+        user.setRating(ratingOfUsers);
         return user;
     }
+
 }
