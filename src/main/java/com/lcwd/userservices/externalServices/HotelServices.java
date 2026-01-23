ef7b2d29e3eac648b4213b51fdce7a74ad9382e7
Maawiya06 <maawiyaameer001@gmail.com>
2026-01-23 19:27:29 +0530

    Merge branch 'main' of https://github.com/Maawiya06/userServices

--- src/main/java/com/lcwd/userservices/externalServices/HotelServices.java
+++ src/main/java/com/lcwd/userservices/externalServices/HotelServices.java
@@ -1,4 +1,13 @@
 package com.lcwd.userservices.externalServices;
 
+import com.lcwd.userservices.entities.Hotel;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name= "HOTELSERVICES")
 public interface HotelServices {
+
+    @GetMapping("/hotels/{hotelId}")
+    Hotel getHotel(@PathVariable("hotelId") String hotelId);
 }
