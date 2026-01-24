77a5bbfdfba27b7688963145419b9e445f3b8e75
Maawiya06 <maawiyaameer001@gmail.com>
2026-01-24 12:44:34 +0530

    Merge branch 'main' of https://github.com/Maawiya06/userServices

--- src/main/java/com/lcwd/userservices/externalServices/RatingServices.java
+++ src/main/java/com/lcwd/userservices/externalServices/RatingServices.java
@@ -2,6 +2,7 @@ package com.lcwd.userservices.externalServices;
 
 import com.lcwd.userservices.entities.Rating;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name= "RATINGSERVICES")
 public interface RatingServices {
@@ -9,6 +10,7 @@ public interface RatingServices {
     //GET
 
     //POST
+    @PostMapping("/ratings")
     Rating createRating();
 
     //PUT
