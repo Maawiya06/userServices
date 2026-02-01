02005fc88ec0b95586ec4b934944d1f10deb5ea1
Maawiya06 <maawiyaameer001@gmail.com>
2026-02-01 23:34:15 +0530

    Merge branch 'main' of https://github.com/Maawiya06/userServices

--- src/main/java/com/lcwd/userservices/controller/UserController.java
+++ src/main/java/com/lcwd/userservices/controller/UserController.java
@@ -2,8 +2,12 @@ package com.lcwd.userservices.controller;
 
 import com.lcwd.userservices.entities.User;
 import com.lcwd.userservices.services.UserServices;
+import com.lcwd.userservices.services.implementation.userServiceImplementation;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.support.FeignHttpClientProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +20,8 @@ public class UserController {
 
     @Autowired
     private UserServices userServices;
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
     // create
     @PostMapping
@@ -39,8 +45,15 @@ public class UserController {
         return ResponseEntity.ok(user2);
     }
 
-    // creating fall back method for circuit breaker
-    public ResponseEntity<User> ratingHotelFallBack(String ){
-
+    // creating fall back method for circuit breaker(fall back method return type and api return type will be same)
+    public ResponseEntity<User> ratingHotelFallBack(String userId, Exception ex){
+        logger.info("fall back is executed beacuse service is down : " , ex.getMessage());
+        User user = User.builder()
+                .userEmail("Dummy@gmail.com")
+                .userName("Dummy")
+                .userAbout("This user is created dummy because some services are down")
+                .userId("GN2490")
+                .build();
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
